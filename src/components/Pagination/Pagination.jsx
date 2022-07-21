3eb8ea9a0eadf0894e8d5112225af4292f10e655
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-21 13:08:17 +0300

    added pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTotalPizzaCount } from "../../api/PizzaService";
+import { setPage } from "../../store/slices/pizzaSlice";
+import st from "./Pagination.module.css";
 
 const Pagination = () => {
+    const [totalPages, setTotalPages] = useState(1);
+    const { limitItems, page } = useSelector((state) => state.pizzaSlice);
+
+    useEffect(() => {
+        getTotalPizzaCount(setTotalPages);
+    }, []);
+
+    const pages = Math.ceil(totalPages / limitItems);
+
+    const dispatch = useDispatch();
+
+    const changePage = (page, variant) => {
+        if (variant === "previous") {
+            page > 1 && dispatch(setPage(page - 1));
+        } else if (variant === "next") {
+            page < pages && dispatch(setPage(page + 1));
+        } else {
+            dispatch(setPage(page));
+        }
+    };
 
     return (
-        <div>
-            pagination
-        </div>
-    )
-}
+        <div className={st.pagination}>
+            <span
+                className={`${st.page} ${page === 1 ? st.inactive : ""}`}
+                onClick={() => changePage(page, "previous")}
+            >
+                {"<"}
+            </span>
 
-export default Pagination
+            {pages > 1 &&
+                [...new Array(pages)].map((_, p) => (
+                    <span
+                        onClick={() => changePage(p + 1, "var")}
+                        className={
+                            p + 1 === page
+                                ? `${st.page} ${st.page__selected}`
+                                : st.page
+                        }
+                        key={p}
+                    >
+                        {p + 1}
+                    </span>
+                ))}
+
+            <span
+                className={`${st.page} ${page === pages && st.inactive}`}
+                onClick={() => changePage(page, "next")}
+            >
+                {">"}
+            </span>
+        </div>
+    );
+};
+
+export default Pagination;
--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -2,21 +2,24 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPizza } from "../api/PizzaService";
 import PizzaItem from "./PizzaItem/PizzaItem";
+import Pagination from "./Pagination/Pagination";
 
 const PizzaList = () => {
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchPizza());
+    const { page } = useSelector((state) => state.pizzaSlice);
 
+    useEffect(() => {
+        dispatch(fetchPizza(page));
         document.title = "Pizza";
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [page]);
 
     const { pizza, status } = useSelector((state) => state.pizzaSlice);
 
     return (
         <div>
+            <Pagination />
             {status === "loading" ? (
                 <p>loading</p>
             ) : (
