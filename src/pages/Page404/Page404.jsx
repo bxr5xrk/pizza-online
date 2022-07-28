1a0f17ccfb3517e073218bb65d114cd6cd19aaef
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-28 22:06:06 +0300

    added empty cart and 404 pages

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import st from "./Page404.module.scss";
 
 const Page404 = () => {
     return (
-        <div>
-            <Link to="/pizza">404</Link>
+        <div className="main">
+            <div className={st.notFound}>
+                <h1>404</h1>
+                <h2>Дана сторінка відсутня. :(</h2>
+                <Link to="/">перейти на головну</Link>
+            </div>
         </div>
     );
 };
