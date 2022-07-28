26e3b928e3ca756d1343dc497bf75f64c01951e3
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-28 15:49:36 +0300

    styled

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -5,32 +5,76 @@ import {
     cartItemDecrement,
     removePizzaFromCart,
 } from "../../store/slices/cartSlice";
+import st from "../../pages/Cart/Cart.module.scss";
 
 const CartItem = ({ id, title, count, image, size, edge, price }) => {
     const dispatch = useDispatch();
 
     return (
-        <div>
-            <img src={image} alt={title} width={50} height={50} />
-            <h3>{title}</h3>
-            <h4>{size}</h4>
-            <h4>{edge}</h4>
-            <h4>{price * count}</h4>
+        <div className={st.item}>
+            <div className={st.item__pizza}>
+                <img src={image} alt={title} width={100} height={100} />
+                <div className={st.pizza__info}>
+                    <h3>{title}</h3>
+                    <div>
+                        <p>{edge}</p>
+                        <p>{size} см</p>
+                    </div>
+                </div>
+            </div>
 
-            <p onClick={() => dispatch(cartItemDecrement({ id, size, edge }))}>
-                -
-            </p>
-            <p>{count}</p>
-            <p onClick={() => dispatch(addPizzaToCart({ id, size, edge }))}>
-                +
-            </p>
-            <p
+            <div className={st.item__count}>
+                <span
+                    onClick={() =>
+                        dispatch(cartItemDecrement({ id, size, edge }))
+                    }
+                >
+                    <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" />
+                    </svg>
+                </span>
+                <h3>{count}</h3>
+                <span
+                    onClick={() => dispatch(addPizzaToCart({ id, size, edge }))}
+                >
+                    <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" />
+                        <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" />
+                    </svg>
+                </span>
+            </div>
+
+            <h3 className={st.item__price}>{price * count} грн.</h3>
+
+            <span
+                className={st.item__remove}
                 onClick={() =>
                     dispatch(removePizzaFromCart({ id, size, edge }))
                 }
             >
-                remove all
-            </p>
+                <svg
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"></path>
+                    <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"></path>
+                </svg>
+            </span>
         </div>
     );
 };
