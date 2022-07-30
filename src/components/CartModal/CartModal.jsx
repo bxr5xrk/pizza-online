eb80ea0726b7580587f9e6fa76ea0bbe0b4b74d2
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-30 13:50:55 +0300

    added modal window for order submit

---
+++ src/components/CartModal/CartModal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import st from "./CartModal.module.scss";
+
+const CartModal = () => {
+    return (
+        <div className={st.wrapper}>
+            modal window <input type="text" placeholder="enter email" />{" "}
+        </div>
+    );
+};
+
+export default CartModal;
