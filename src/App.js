d1db48595cf4cb59115ca66d4c6ea1e9a1970175
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-08-03 16:03:47 +0300

    added private routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header/Header";
-import Home from "./pages/PublicPages/HomePage/HomePage";
-import HomePage from "./pages/PrivatePages/HomePage/HomePage";
-import OrdersPage from "./pages/PrivatePages/OrdersPage/OrdersPage";
-import StatisticsPage from "./pages/PrivatePages/StatisticsPage/StatisticsPage";
-import Cart from "./pages/PublicPages/Cart/Cart";
-import LoginPage from "./pages/PublicPages/LoginPage/LoginPage";
-import Page404 from "./pages/PublicPages/Page404/Page404";
-import PizzaPage from "./pages/PublicPages/PizzaPage/PizzaPage";
-import PizzasPage from "./pages/PublicPages/PizzasPage";
+import Home from "./pages/HomePage/HomePage";
+import PrivateHomePage from "./pages/PrivateHomePage/PrivateHomePage";
+import OrdersPage from "./pages/OrdersPage/OrdersPage";
+import StatisticsPage from "./pages/StatisticsPage/StatisticsPage";
+import Cart from "./pages/Cart/Cart";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import Page404 from "./pages/Page404/Page404";
+import PizzaPage from "./pages/PizzaPage/PizzaPage";
+import PizzasPage from "./pages/PizzasPage";
 import { selectAuth } from "./store/slices/authSlice";
+import Layout from "./layout/Layout";
+import Privatelayout from "./layout/Privatelayout";
 
 function App() {
     const { isAuth } = useSelector(selectAuth);
 
-    useEffect(() => {
-        localStorage.setItem("isAuth", isAuth);
-    }, [isAuth]);
-
     return (
         <>
             {isAuth ? (
-                <>
+                <Privatelayout>
                     <Routes>
-                        <Route path="/" element={<HomePage />} />
+                        <Route path="/" element={<PrivateHomePage />} />
                         <Route path="/orders" element={<OrdersPage />} />
                         <Route
                             path="/statistics"
@@ -34,11 +29,9 @@ function App() {
                         />
                         <Route path="*" element={<Navigate to="/" replace />} />
                     </Routes>
-                </>
+                </Privatelayout>
             ) : (
-                <>
-                    <Header />
-
+                <Layout>
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route
@@ -54,9 +47,7 @@ function App() {
                         />
                         <Route path="*" element={<Page404 />} />
                     </Routes>
-
-                    <Footer />
-                </>
+                </Layout>
             )}
         </>
     );
