b5a7dac39aede5eb930cfc12e6e7eaa8d689dfa4
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-08-04 11:56:58 +0300

    fixed skeleton incorrect display

--- src/components/PizzaSkeleton.jsx
+++ src/components/PizzaSkeleton.jsx
@@ -6,17 +6,17 @@ const PizzaSkeleton = (props) => (
     <div className={st.item}>
         <ContentLoader
             speed={2}
-            width={365}
-            height={480}
-            viewBox="0 0 365 480"
+            width={304}
+            height={495}
+            viewBox="0 0 304 495"
             backgroundColor="#f3f3f3"
             foregroundColor="#ecebeb"
             {...props}
         >
-            <circle cx="178" cy="127" r="125" />
-            <rect x="34" y="259" rx="5" ry="5" width="280" height="36" />
-            <rect x="34" y="301" rx="5" ry="5" width="280" height="126" />
-            <rect x="34" y="435" rx="0" ry="0" width="280" height="48" />
+            <circle cx="156" cy="135" r="120" />
+            <rect x="26" y="310" rx="10" ry="10" width="260" height="126" />
+            <rect x="27" y="450" rx="10" ry="10" width="260" height="48" />
+            <rect x="26" y="265" rx="10" ry="10" width="260" height="36" />
         </ContentLoader>
     </div>
 );
